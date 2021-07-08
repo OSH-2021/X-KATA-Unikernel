06c8addf518f0dcca11c92c29cac58ec4ed5a363
Sea-USTC <1477989047@qq.com>
2021-07-08 20:39:27 +0800

    ppt redir

--- deps.sh
+++
@@ -1,9 +0,0 @@
-ops build $1
-sudo virt-install --connect=qemu:///system \
-    --name=$2 \
-    --ram=128 \
-    --vcpu=1 \
-    --disk path=/home/$USER/.ops/images/$1.img,format=raw \
-    --import \
-    --network network:default \
-    --vnc
